e7cdb3153ca248100f8ca84a64cd3a8f63443424
kidus <Kidkasam@users.noreply.github.com>
2026-01-17 10:56:01 +0300

    implement Django views and URL configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'relationship_app'
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/', views.LibraryListView.as_view(), name='library_list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
+    # Authentication URLs
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Book, Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based view: List all books
 def list_books(request):
@@ -34,3 +36,14 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         # Add additional context if needed
         return context
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('relationship_app:list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
