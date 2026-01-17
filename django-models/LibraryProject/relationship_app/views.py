9e2570ecd6164ea9651e1aa339564952635177e7
kidus <Kidkasam@users.noreply.github.com>
2026-01-17 11:13:48 +0300

    implement custom permissions for Book model with role-based CRUD restrictions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
